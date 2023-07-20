cda5e377b3ddb119630dfcd76354f30562530cc8
beginner1986 <beginner1986@gmail.com>
2023-07-20 22:28:22 +0200

    React hook style counter implemented

--- src/features/Counter.tsx
+++ src/features/Counter.tsx
@@ -1,10 +1,17 @@
+import { useState } from "react"
+
 export default function Counter() {
+    const [value, setValue] = useState(0);
+
+    const increment = () => { setValue(value + 1) };
+    const decrement = () => { setValue(value - 1) };
+
     return (
         <div id='counter'>
-            <h1>value</h1>
+            <h1>{value}</h1>
             <div>
-                <button className='btn'>+</button>
-                <button className='btn'>-</button>
+                <button onClick={increment} className='btn'>+</button>
+                <button onClick={decrement} className='btn'>-</button>
             </div>
         </div>
     )
