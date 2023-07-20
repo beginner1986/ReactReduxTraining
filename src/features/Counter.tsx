30f2bcd5bfaddad733b76cb101d2c864461eca4d
beginner1986 <beginner1986@gmail.com>
2023-07-20 22:48:55 +0200

    Counter changed to redux toolkit style

--- src/features/Counter.tsx
+++ src/features/Counter.tsx
@@ -1,17 +1,16 @@
-import { useState } from "react"
+import { useAppDispatch, useAppSelector } from "../store/configureStore"
+import { decrement, increment } from "./counterSlice";
 
 export default function Counter() {
-    const [value, setValue] = useState(0);
-
-    const increment = () => { setValue(value + 1) };
-    const decrement = () => { setValue(value - 1) };
+    const dispatch = useAppDispatch();
+    const {value} = useAppSelector(state => state.counter);
 
     return (
         <div id='counter'>
             <h1>{value}</h1>
             <div>
-                <button onClick={increment} className='btn'>+</button>
-                <button onClick={decrement} className='btn'>-</button>
+                <button onClick={() => dispatch(increment())} className='btn'>+</button>
+                <button onClick={() => dispatch(decrement())} className='btn'>-</button>
             </div>
         </div>
     )
--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,17 @@ import reportWebVitals from './reportWebVitals';
 import App from './layout/App';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/reutes';
+import { Provider } from 'react-redux';
+import { store } from './store/configureStore';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
