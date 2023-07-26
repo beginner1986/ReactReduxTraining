52fa6c7f8d9f580d85aa1796da7eee9a32d9d4a0
beginner1986 <beginner1986@gmail.com>
2023-07-26 21:44:31 +0200

    AsyncCounterSlice reducers implemented

--- src/features/AsyncCounter/aysncCounterSlice.ts
+++ src/features/AsyncCounter/aysncCounterSlice.ts
@@ -1,14 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { CounterState } from "../counter/counterSlice";
 
-const initialState: CounterState = {
-    value: 0
+interface AsyncCounterState {
+    value: number;
+    loading: boolean;
+}
+
+const initialState: AsyncCounterState = {
+    value: 0,
+    loading: false
 }
 
 export const asyncCounterSlice = createSlice({
     name: 'asyncCounter',
     initialState,
     reducers: {
-        
+        counterAsyncUpdate: (state) => {
+            state.loading = true;
+        },
+        incrementAsyncSuccess: (state) => {
+            state.value += 1;
+            state.loading = false;
+        },
+        decrementAsyncSuccess: (state) => {
+            state.value -= 1;
+            state.loading = false;
+        },
+        counterAsyncUpdateFailure: (state) => {
+            state.loading = false;
+        }
     }
-})
+});
+
+export const { counterAsyncUpdate, incrementAsyncSuccess, decrementAsyncSuccess, counterAsyncUpdateFailure } = asyncCounterSlice.actions;
